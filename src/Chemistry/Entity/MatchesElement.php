af771c281f310bf5709abaa63444ed0e57f9d724
Łukasz Fiszer <45523649+lukasz-fiszer@users.noreply.github.com>
2017-06-10 23:57:45 +0200

    Add findMatchingElement() method to MatchesElement trait
    Add findMatchingElement() method to MatchesElement trait, that finds matching element among elements collection using callback on collection items
    Add tests to MatchesElementTest, based on callFindMatchingElement() helper method and @expectedException of PHPUnit error for callback that causes errors and exceptions

--- src/Chemistry/Entity/MatchesElement.php
+++ src/Chemistry/Entity/MatchesElement.php
@@ -23,4 +23,24 @@ trait MatchesElement
 		return true;
 		//return array_intersect_assoc($elementData, $element) == $element;
 	}
+
+	/**
+	 * Find matching element among elements data collection using callback on collection items
+	 * 
+	 * @param  array    $element                element key-value pairs
+	 * @param  array    $elementsDataCollection elements data collection
+	 * @param  callable $toElementData          optional callback to use on collection items
+	 * @param  bool     $returnElementData      optional, true if element data after callback must be returned, false if original collection entry must be returned
+	 * @return array|null  matched element data or null otherwise
+	 */
+	protected function findMatchingElement(array $element, array $elementsDataCollection, callable $toElementData = null, bool $returnElementData = false){
+		$toElementData = $toElementData ?: function($arg){return $arg;};
+		foreach($elementsDataCollection as $elementDataEntry){
+			$elementData = $toElementData($elementDataEntry);
+			if($this->checkIfElementMatchesData($element, $elementData)){
+				return $returnElementData ? $elementData : $elementDataEntry;
+			}
+		}
+		return null;
+	}
 }
--- tests/Chemistry/Entity/MatchesElementTest.php
+++ tests/Chemistry/Entity/MatchesElementTest.php
@@ -33,4 +33,68 @@ class MatchesElementTest extends \PHPUnit\Framework\TestCase
 			[[], [], true],
 		];
 	}
+
+	public function testFindMatchingElement(){
+		$elementMatcherMock = $this->getMockForTrait(MatchesElement::class);
+		$toElementData = function($elementEntry){return $elementEntry['element'];};
+		$e1 = ['symbol' => 'H'];
+		$e2 = ['symbol' => 'H', 'name' => 'Hydrogen_changed_name'];
+		$e3 = ['symbol' => 'FicSym'];
+		$e4 = ['non_existent_key' => 'value'];
+		$dataJsonPath = realpath(dirname(__FILE__)).'/../../../res/PeriodicTableJSON.json';
+		$c1 = json_decode(file_get_contents($dataJsonPath), true)['elements'];
+		$c2 = [
+			['element' => $e1, 'occurences' => 2],
+			['element' => $e2, 'occurences' => 4],
+		];
+
+		$matched = $this->invokeMethod($elementMatcherMock, 'findMatchingElement', [$e1, $c1]);
+		$this->assertEquals($c1[0], $matched);
+		$matched2 = $this->invokeMethod($elementMatcherMock, 'findMatchingElement', [$e1, $c1, null, true]);
+		$this->assertEquals($c1[0], $matched2);
+
+		/*$matched3 = $this->invokeMethod($elementMatcherMock, 'findMatchingElement', [$e1, $c1, null, true]);
+		$this->assertEquals($c1[0], $matched2);*/
+
+		$this->callFindMatchingElement(null, [$e2, $c1]);
+		$this->callFindMatchingElement(null, [$e2, $c1, null, true]);
+		$this->callFindMatchingElement(null, [$e3, $c1]);
+		$this->callFindMatchingElement(null, [$e3, $c1], null, true);
+		$this->callFindMatchingElement(null, [$e4, $c1]);
+		$this->callFindMatchingElement(null, [$e4, $c1, null, true]);
+
+		$this->callFindMatchingElement($e1, [$e1, $c2, $toElementData, true]);
+		$this->callFindMatchingElement(['element' => $e1, 'occurences' => 2], [$e1, $c2, $toElementData]);
+		$this->callFindMatchingElement(null, [$e3, $c2, $toElementData]);
+		$this->callFindMatchingElement(null, [$e3, $c2, $toElementData, true]);
+		$this->callFindMatchingElement(null, [$e4, $c2, $toElementData]);
+		$this->callFindMatchingElement(null, [$e4, $c2, $toElementData, true]);
+
+		$this->callFindMatchingElement(null, [$e3, $c2]);
+		$this->callFindMatchingElement(null, [$e3, $c2, null, true]);
+		$this->callFindMatchingElement(null, [$e4, $c2]);
+		$this->callFindMatchingElement(null, [$e4, $c2, null, true]);
+	}
+
+	//protected function testFindMatchingElementCall(){
+	protected function callFindMatchingElement($expected, $arguments){
+		$elementMatcherMock = $this->getMockForTrait(MatchesElement::class);
+		$matched = $this->invokeMethod($elementMatcherMock, 'findMatchingElement', $arguments);
+		$this->assertEquals($expected, $matched);
+	}
+
+	/**
+	 * @e/xpectedException Exception
+	 * @expectedException \PHPUnit\Framework\Error\Error
+	 */
+	public function testFindMatchingElementCallableException(){
+		$elementMatcherMock = $this->getMockForTrait(MatchesElement::class);
+		$e1 = ['symbol' => 'H'];
+		$e2 = ['symbol' => 'H', 'name' => 'Hydrogen_changed_name'];
+		$c2 = [
+			['element' => $e1, 'occurences' => 2],
+			['element' => $e2, 'occurences' => 4],
+		];
+		$this->invokeMethod($elementMatcherMock, 'findMatchingElement', [$e1, $c2, function($entry){return $entry['element_changed_key'];}]);
+	}
 }
