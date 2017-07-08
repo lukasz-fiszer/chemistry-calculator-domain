09a8451b8df40e6065e9ee55556d0a1087993272
Łukasz Fiszer <45523649+lukasz-fiszer@users.noreply.github.com>
2017-07-08 20:14:37 +0200

    Add ParserExceptionBuilder and its test
    Add ParserExceptionBuilder immutable builder and its test
    ParserExceptionBuilder is an immutable builder that takes care of parser exception building and its message, code, previous, parser input/position/line/column properties
    Add ParserExceptionBuilder to test the builder

---
+++ src/Chemistry/Parser/ParserExceptionBuilder.php
@@ -0,0 +1,212 @@
+<?php
+
+namespace ChemCalc\Domain\Chemistry\Parser;
+
+use Exception;
+
+/**
+ * Chemistry reaction equation
+ * Parser exception builder, immutable object
+ */
+class ParserExceptionBuilder
+{
+	/**
+	 * Input to the parser stream
+	 * 
+	 * @var string
+	 */
+	protected $parserInput;
+
+	/**
+	 * Current position in the parser stream where exception was thrown
+	 * 
+	 * @var integer
+	 */
+	protected $parserPosition;
+
+	/**
+	 * Current line in the parser stream where exception was thrown
+	 * 
+	 * @var integer
+	 */
+	protected $parserLine;
+
+	/**
+	 * Current column in the parser stream where exception was thrown
+	 * 
+	 * @var integer
+	 */
+	protected $parserColumn;
+
+	/**
+	 * Exception message
+	 * 
+	 * @var string
+	 */
+	protected $message;
+
+	/**
+	 * Exception ode
+	 * 
+	 * @var int
+	 */
+	protected $code;
+
+	/**
+	 * Previous exception
+	 * 
+	 * @var Exception
+	 */
+	protected $previous;
+
+	/**
+	 * Exception codes array
+	 * 
+	 * @var array
+	 */
+	protected $codes = [
+
+	];
+
+	/**
+	 * Construct new parser exception builder
+	 * 
+	 * @param string|null    $message  exception message
+	 * @param integer|null   $code     exception code
+	 * @param Exception|null $previous previous exception
+	 */
+	public function __construct(string $message = null, int $code = 0, Exception $previous = null){
+		$this->message = $message;
+		$this->code = $code;
+		$this->previous = $previous;
+	}
+
+	/**
+	 * Build parser exception
+	 * 
+	 * @return ParserException built parser exception
+	 */
+	public function build(){
+		$message = $this->buildMessage();
+		return new ParserException($message, $this->parserInput, $this->parserPosition, $this->parserLine, $this->parserColumn, $this->code, $this->previous);
+	}
+
+	/**
+	 * Build exception message
+	 * 
+	 * @return string|null exception message
+	 */
+	protected function buildMessage(){
+		if($this->message === null){
+			return $this->message;
+		}
+		$line = $this->parserLine !== null ? 'line: '.$this->parserLine : null;
+		$column = $this->parserColumn !== null ? 'column: '.$this->parserColumn : null;
+		$implode = [$line, $column];
+		$implode = array_filter($implode, function($entry){
+			return $entry !== null;
+		});
+		return $this->message.' ('.implode(', ', $implode).')';
+	}
+
+	/**
+	 * Return new builder instance with given message
+	 * 
+	 * @param  string $message exception message
+	 * @return self new exception builder instance
+	 */
+	public function withMessage(string $message){
+		$new = clone $this;
+		$new->message = $message;
+		return $new;
+	}
+
+	/**
+	 * Return new builder instance with given code
+	 * 
+	 * @param  int $code exception code
+	 * @return self new exception builder instance
+	 */
+	public function withCode(int $code){
+		$new = clone $this;
+		$new->code = $code;
+		return $new;
+	}
+
+	/**
+	 * Return new builder instance with given code, specified by its key
+	 * 
+	 * @param  string $key exception code key
+	 * @throws Exception exception thrown when given key does not exist
+	 * @return self new exception builder instance
+	 */
+	public function withCodeByKey(string $key){
+		$new = clone $this;
+		if(!isset($this->codes[$key])){
+			throw new Exception('Unknown code key '.$key);
+		}
+		$new->code = $this->codes[$key];
+		return $new;
+	}
+
+
+	/**
+	 * Return new builder instance with previous exception
+	 * 
+	 * @param  Exception $previous previous exception
+	 * @return self new exception builder instance
+	 */
+	public function withPreviousException(Exception $previous){
+		$new = clone $this;
+		$new->previous = $previous;
+		return $new;
+	}
+
+	/**
+	 * Return new builder instance with parser input
+	 * 
+	 * @param  string $input parser input
+	 * @return self new exception builder instance
+	 */
+	public function withParserInput(string $input){
+		$new = clone $this;
+		$new->parserInput = $input;
+		return $new;
+	}
+
+	/**
+	 * Return new builder instance with parser position
+	 * 
+	 * @param  int $position parser position
+	 * @return self new exception builder instance
+	 */
+	public function withParserPosition(int $position){
+		$new = clone $this;
+		$new->parserPosition = $position;
+		return $new;
+	}
+
+	/**
+	 * Return new builder instance with parser line
+	 * 
+	 * @param  int $line parser line
+	 * @return self new exception builder instance
+	 */
+	public function withParserLine(int $line){
+		$new = clone $this;
+		$new->parserLine = $line;
+		return $new;
+	}
+
+	/**
+	 * Return new builder instance with parser column
+	 * 
+	 * @param  int $column parser column
+	 * @return self new exception builder instance
+	 */
+	public function withParserColumn(int $column){
+		$new = clone $this;
+		$new->parserColumn = $column;
+		return $new;
+	}
+}
---
+++ tests/Chemistry/Parser/ParserExceptionBuilderTest.php
@@ -0,0 +1,116 @@
+<?php
+
+namespace ChemCalc\Domain\Tests\Chemistry\Parser;
+
+use ChemCalc\Domain\Chemistry\Parser\ParserException;
+use ChemCalc\Domain\Chemistry\Parser\ParserExceptionBuilder;
+use Exception;
+
+class ParserExceptionBuilderTest extends \PHPUnit\Framework\TestCase
+{
+	public function testConstructorPropertiesInjection(){
+		$builder = new ParserExceptionBuilder();
+		$this->assertAttributeEquals(null, 'message', $builder);
+		$this->assertAttributeEquals(0, 'code', $builder);
+		$this->assertAttributeEquals(null, 'previous', $builder);
+
+		$prev = new Exception();
+		$builder = new ParserExceptionBuilder('exception message', 10, $prev);
+		$this->assertAttributeEquals('exception message', 'message', $builder);
+		$this->assertAttributeEquals(10, 'code', $builder);
+		$this->assertAttributeEquals($prev, 'previous', $builder);
+	}
+
+	public function testBuildMethod(){
+		$builder = new ParserExceptionBuilder();
+		$exBuild = $builder->build();
+		$ex = new ParserException();
+		$this->assertEquals($ex, $exBuild);
+
+		$prev = new Exception('previous msg');
+		$builder2 = new ParserExceptionBuilder('message', 0, $prev);
+		$builder2 = $builder2->withMessage('new msg')->withCode(2)->withParserInput('parser input')->withParserLine(1)->withParserColumn(2);
+		$exBuild2 = $builder2->build();
+		$ex2 = new ParserException('new msg (line: 1, column: 2)', 'parser input', null, 1, 2, 2, $prev);
+		$this->assertEquals($ex2, $exBuild2);
+
+		$builder2 = new ParserExceptionBuilder();
+		$builder2 = $builder2->withMessage('new msg')->withParserLine(1);
+		$exBuild2 = $builder2->build();
+		$ex2 = new ParserException('new msg (line: 1)', null, null, 1);
+		$this->assertEquals($ex2, $exBuild2);
+
+		$builder2 = new ParserExceptionBuilder();
+		$builder2 = $builder2->withMessage('new msg')->withParserColumn(2);
+		$exBuild2 = $builder2->build();
+		$ex2 = new ParserException('new msg (column: 2)', null, null, null, 2);
+		$this->assertEquals($ex2, $exBuild2);
+	}
+
+	public function testWithMessage(){
+		$builder = new ParserExceptionBuilder();
+		$builder = $builder->withMessage('msg');
+		$this->assertAttributeEquals('msg', 'message', $builder);
+		$builder2 = $builder->withMessage('msg 2');
+		$this->assertAttributeEquals('msg 2', 'message', $builder2);
+	}
+
+	public function testWithCode(){
+		$builder = new ParserExceptionBuilder();
+		$builder = $builder->withCode(2);
+		$this->assertAttributeEquals(2, 'code', $builder);
+		$builder2 = $builder->withCode(4);
+		$this->assertAttributeEquals(4, 'code', $builder2);
+	}
+
+	public function testWithCodeByKey(){
+		$this->markTestIncomplete();
+		$builder = new ParserExceptionBuilder();
+		$builder = $builder->withCode(2);
+		$this->assertAttributeEquals(2, 'code', $builder);
+		$builder2 = $builder->withCode(4);
+		$this->assertAttributeEquals(4, 'code', $builder2);
+	}
+
+	public function testWithPreviousException(){
+		$builder = new ParserExceptionBuilder();
+		$ex1 = new Exception();
+		$ex2 = new Exception('previous');
+		$builder = $builder->withPreviousException($ex1);
+		$this->assertAttributeEquals($ex1, 'previous', $builder);
+		$builder2 = $builder->withPreviousException($ex2);
+		$this->assertAttributeEquals($ex2, 'previous', $builder2);
+	}
+
+	public function testWithParserInput(){
+		$builder = new ParserExceptionBuilder();
+		$builder = $builder->withParserInput('parser input 1');
+		$this->assertAttributeEquals('parser input 1', 'parserInput', $builder);
+		$builder2 = $builder->withParserInput('parser input 2');
+		$this->assertAttributeEquals('parser input 2', 'parserInput', $builder2);
+	}
+
+	public function testWithParserPosition(){
+		$builder = new ParserExceptionBuilder();
+		$builder = $builder->withParserPosition(10);
+		$this->assertAttributeEquals(10, 'parserPosition', $builder);
+		$builder2 = $builder->withParserPosition(20);
+		$this->assertAttributeEquals(20, 'parserPosition', $builder2);
+	}
+
+	public function testWithParserLine(){
+		$builder = new ParserExceptionBuilder();
+		$builder = $builder->withParserLine(100);
+		$this->assertAttributeEquals(100, 'parserLine', $builder);
+		$builder2 = $builder->withParserLine(200);
+		$this->assertAttributeEquals(200, 'parserLine', $builder2);
+	}
+
+	public function testWithParserColumn(){
+		$builder = new ParserExceptionBuilder();
+		$builder = $builder->withParserColumn(5);
+		$this->assertAttributeEquals(5, 'parserColumn', $builder);
+		$builder2 = $builder->withParserColumn(40);
+		$this->assertAttributeEquals(40, 'parserColumn', $builder2);
+	}
+}
