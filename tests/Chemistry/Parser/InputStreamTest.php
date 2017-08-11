082d6279ac4fc1050c1b60dba78615a6b034a881
Łukasz Fiszer <45523649+lukasz-fiszer@users.noreply.github.com>
2017-08-11 23:44:06 +0200

    Clean InputStreamTest
    Clean and refactor InputStreamTest

--- tests/Chemistry/Parser/InputStreamTest.php
+++ tests/Chemistry/Parser/InputStreamTest.php
@@ -32,27 +32,16 @@ class InputStreamTest extends \PHPUnit\Framework\TestCase
 
 	public function testStreamMethods(){
 		$inputStream = new InputStream("abcd\nabcd\ntest\n\t ab", self::$exceptionBuilderMock);
-		$this->assertAttributeEquals("abcd\nabcd\ntest\n\t ab", 'input', $inputStream);
-		$this->assertEquals($this->buildInputContext("abcd\nabcd\ntest\n\t ab", 0, 0, 0), $inputStream->getContext());
+		$this->streamValuesTest("abcd\nabcd\ntest\n\t ab", 0, 0, 0, false, $inputStream);
 
 		$this->assertEquals('a', $inputStream->peek());
 		$this->assertEquals('a', $inputStream->next());
 		$this->streamValuesTest("abcd\nabcd\ntest\n\t ab", 1, 0, 1, false, $inputStream);
-		/*$this->assertAttributeEquals("abcd\nabcd\ntest\n\t ab", 'input', $inputStream);
-		$this->assertAttributeEquals(1, 'position', $inputStream);
-		$this->assertAttributeEquals(0, 'line', $inputStream);
-		$this->assertAttributeEquals(1, 'column', $inputStream);
-		$this->assertEquals(false, $inputStream->eof());
-		$this->assertEquals($this->buildInputContext("abcd\nabcd\ntest\n\t ab", 1, 0, 1), $inputStream->getContext());*/
 
 		$this->assertEquals('bcd', $inputStream->next().$inputStream->next().$inputStream->next());
 		$this->assertEquals("\n", $inputStream->peek());
 		$this->assertEquals("\n", $inputStream->next());
 		$this->streamValuesTest("abcd\nabcd\ntest\n\t ab", 5, 1, 0, false, $inputStream);
-		/*$this->assertAttributeEquals(5, 'position', $inputStream);
-		$this->assertAttributeEquals(1, 'line', $inputStream);
-		$this->assertAttributeEquals(0, 'column', $inputStream);
-		$this->assertEquals($this->buildInputContext("abcd\nabcd\ntest\n\t ab", 5, 1, 0), $inputStream->getContext());*/
 
 		for($i = 0; $i < 10; $i++){
 			$inputStream->next();
@@ -66,10 +55,7 @@ class InputStreamTest extends \PHPUnit\Framework\TestCase
 		$this->assertEquals(null, $inputStream->next());
 		$this->assertEquals(null, $inputStream->next());
 		$this->assertEquals(true, $inputStream->eof());
-		$this->assertAttributeEquals(19, 'position', $inputStream);
-		$this->assertAttributeEquals(3, 'line', $inputStream);
-		$this->assertAttributeEquals(4, 'column', $inputStream);
-		$this->assertEquals($this->buildInputContext("abcd\nabcd\ntest\n\t ab", 19, 3, 4), $inputStream->getContext());
+		$this->streamValuesTest("abcd\nabcd\ntest\n\t ab", 19, 3, 4, true, $inputStream);
 	}
 
 	public function testExceptionThrown(){
@@ -81,12 +67,7 @@ class InputStreamTest extends \PHPUnit\Framework\TestCase
 		catch(Exception $e){
 			$catched = true;
 			$this->assertAttributeEquals('message (line: 0, column: 0)', 'message', $e);
-			/*$this->assertAttributeEquals('test', 'parserInput', $e);
-			$this->assertAttributeEquals(0, 'parserPosition', $e);
-			$this->assertAttributeEquals(0, 'parserLine', $e);
-			$this->assertAttributeEquals(0, 'parserColumn', $e);*/
-			$c = (object) ['input' => 'test', 'position' => 0, 'line' => 0, 'column' => 0];
-			$this->assertAttributeEquals($c, 'parserContext', $e);
+			$this->assertAttributeEquals($this->buildInputContext('test', 0, 0, 0), 'parserContext', $e);
 		}
 		if(!$catched){
 			$this->fail('Input stream had to throw exception');
